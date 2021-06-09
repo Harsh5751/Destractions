9e3a9892cc2d4e6eefb97b1b71cbe52171817e30
Harsh Patel <42149411+Harsh5751@users.noreply.github.com>
2021-06-09 14:05:35 -0400

    Update home.js

--- public/JS/home.js
+++ public/JS/home.js
@@ -1,10 +1,3 @@
-/*
-File: home.js
-Authors: Harsh Patel, Matthew Braden
-Last Edited: October 4, 2019
-Version: 1.0
-*/
-
 //background image carousel methods
 $('.carousel').carousel({
   interval: 3000
